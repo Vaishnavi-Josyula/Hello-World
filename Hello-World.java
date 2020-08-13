205eec83364545cb4080bb0a9abafe8116ddfaf5
Vaishnavi-Josyula <43019257+Vaishnavi-Josyula@users.noreply.github.com>
2020-08-12 19:14:48 -0700

    Update Hello-World.java

--- Hello-World.java
+++ Hello-World.java
@@ -4,7 +4,7 @@ public class Main
             //the following code prints five random pieces of information about me.
 	    System.out.println("My name is Vaishnavi Josyula.");
 	    System.out.println("I am a junior at Milpitas High.");
-	    System.out.println("I have learned how to paint and do calligraphy but occassionally practice the latter right now.");
+	    System.out.println("I have learned how to paint and do calligraphy and occassionally practice the latter.");
 	    System.out.println("I enjoy reading comics.");
 	    System.out.println("I also enjoy singing.");	
 	}
