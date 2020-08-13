ab96635746170de5048e8d9de7073248a6a1191b
Vaishnavi-Josyula <43019257+Vaishnavi-Josyula@users.noreply.github.com>
2020-08-12 18:48:38 -0700

    Update Hello-World.java

--- Hello-World.java
+++ Hello-World.java
@@ -1,11 +1,11 @@
 public class Main
 {
 	public static void main(String[] args) {
-            //the following code prints fice pieces of information about me
+            //the following code prints five random pieces of information about me.
 	    System.out.println("My name is Vaishnavi Josyula.");
-	    System.out.println("I am a junior.");
-	    System.out.println("I was born on the first of January.");
+	    System.out.println("I am a junior at Milpitas High.");
+	    System.out.println("I have learned how to paint and do calligraphy but occassionally practice the latter right now.");
 	    System.out.println("I enjoy reading comics.");
-	    System.out.println("I also enjoy singing!");	
+	    System.out.println("I also enjoy singing.");	
 	}
 }
