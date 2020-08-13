b7e6ffb0ececee87423172a9695c6e5b7ec2e3c9
Vaishnavi-Josyula <43019257+Vaishnavi-Josyula@users.noreply.github.com>
2020-08-12 18:37:55 -0700

    Add files via upload

---
+++ Hello-World.java
@@ -0,0 +1,19 @@
+/******************************************************************************
+
+                            Online Java Compiler.
+                Code, Compile, Run and Debug java program online.
+Write your code in this editor and press "Run" button to execute it.
+
+*******************************************************************************/
+
+public class Main
+{
+	public static void main(String[] args) {
+	    System.out.println("My name is Vaishnavi.");
+		System.out.println("I am a junior. ");
+	    System.out.println("I was born on the first of January.");
+	    System.out.println("I enjoy reading comics.");
+	    System.out.println("I also enjoy singing!");
+		
+	}
+}
